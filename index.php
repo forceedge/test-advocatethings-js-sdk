31e51cc0e53ad744c16f893b129fbfc2736ac7b4
wahab qureshi <wahab.qureshi@digitalanimal.com>
2015-04-07 11:54:04 +0100

    Get snippet deployed

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <html>
 
 <head>
+<script id="advocate-things-script"
+        src="https://advocate-things.divshot.com/at-sdk-0.0.1.js?key=YOUR_KEY"
+        type="text/javascript"></script>
+
+<script>
+jQuery(function () {
+    // DOM is loaded
+         AT.sendTouchpoint('page_load', {
+                 _at: { user_id: jQuery('#name').val() }
+         })
+});
 </head>
 
 <body>
