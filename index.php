d262c2cc12b151dd4b48d93eaea093bc20e03f2f
wahab qureshi <wahab.qureshi@digitalanimal.com>
2015-04-07 12:17:27 +0100

    Updated url

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script id="advocate-things-script"
-        src="https://advocate-things.divshot.com/at-sdk-0.0.1.js?key=e2a718d19b7d8fe34bf070e282dfdde9"
+        src="https://d22stxronnwc65.cloudfront.net/sdk.js?key=e2a718d19b7d8fe34bf070e282dfdde9"
         type="text/javascript"></script>
 
 <script>
