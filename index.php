f12f62205cc643ac51f86b5f18c24a9409703ae2
wahab qureshi <wahab.qureshi@digitalanimal.com>
2015-04-07 12:29:56 +0100

    Back to the way copied

--- index.php
+++ index.php
@@ -2,6 +2,17 @@
 
 <head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript">
+window.advocate_things_data = {
+   _at: {
+     user_id: '56'
+   },
+   page: {
+     id: '23',
+     category: 'sports'
+  }
+};
+</script>
 <script id="advocate-things-script"
         src="https://d22stxronnwc65.cloudfront.net/sdk.js?key=e2a718d19b7d8fe34bf070e282dfdde9"
         type="text/javascript"></script>
@@ -11,7 +22,7 @@ jQuery(function () {
     // DOM is loaded
          AT.sendTouchpoint('page_load', {
                  _at: { user_id: jQuery('#name').val() }
-         });
+         })
 });
 </script>
 </head>
