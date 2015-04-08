61680a5fe7469951da69c65b3173d07845ef598f
wahab qureshi <wahab.qureshi@digitalanimal.com>
2015-04-08 14:49:09 +0100

    Notifications handler

--- notifications/index.php
+++ notifications/index.php
@@ -3,12 +3,12 @@
 error_reporting(E_ALL);
 ini_set('error_reporting', E_ALL);
 
-if (isset($_POST)) {
-	$resource = fopen('./tmp.data', '+w');
-	fwrite($resource, print_r($_POST), true) . '<br />');
+if (isset($_POST) and $_POST) {
+	$resource = fopen(__DIR__ . '/data.php', 'a+');
+	fwrite($resource, print_r($_POST, true) . '<br />');
 	fclose($resource);
 } else {
-	$data = file_get_contents('./tmp.data', true);
+	$data = file_get_contents(__DIR__ . '/data.php', true);
 	print_r($data);
 }
 
