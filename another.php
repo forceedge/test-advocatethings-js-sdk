c83fc278cd145729c1107dd0efb72f046796db3c
wahab qureshi <wahab.qureshi@digitalanimal.com>
2015-04-08 14:12:47 +0100

    not give me a thingy back

--- another.php
+++ another.php
@@ -1,6 +1,13 @@
 <html>
 <head>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script>
+window.advocate_things_data = {
+   _at: {
+     touchpointName: 'sameUrlTouchpointandSharepoint'
+   }
+};
+</script>
 <script id="advocate-things-script"
         src="https://d22stxronnwc65.cloudfront.net/sdk.js?key=e2a718d19b7d8fe34bf070e282dfdde9"
         type="text/javascript"></script>
